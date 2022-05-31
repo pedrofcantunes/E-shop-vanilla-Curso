6c5d8910c2d47f8cab94257f3f1278cee523ce8e
Pedro Antunes <pedrofca@yahoo.com.br>
2022-05-31 00:02:40 -0300

    Created pages

--- src/js/elements.js
+++ src/js/elements.js
@@ -7,10 +7,21 @@ App.elements = {
     },
     main: {
         container: document.createElement("div"),
+
+    main: {
+        container: document.createElement("div"),
         bg: document.createElement("img"),
         h1: document.createElement("h1"),
         p: document.createElement("p"),
         items: [],
+    },
+    checkout: {
+        container: document.createElement("div"),
+        title: document.createElement("h1"),
+        items: [],
+        confirmBtnContainer: document.createElement("div"),
+        confirmBtn: document.createElement("button"),
+    },
     },
     footer: {
         container: document.createElement("div"),
